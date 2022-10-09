63b01408c51e4ac176a73d2470ff9780795d2ed6
GrodVidar <vexxart@gmail.com>
2022-10-09 14:54:36 +0200

    added user settings templates and logic

--- day_counter/forms.py
+++ day_counter/forms.py
@@ -28,6 +28,7 @@ class CounterForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
