60d9c584742d5148fa9ed63f739e8cd0f8869ced
GrodVidar <vexxart@gmail.com>
2023-07-20 18:08:17 +0200

    setting dev to sandbox_mode

--- grodvidar/settings/dev.py
+++ grodvidar/settings/dev.py
@@ -21,7 +21,7 @@ DATABASES = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'day_counter/media')
 
-SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_SANDBOX_MODE_IN_DEBUG = True
 
 DOMAIN = '127.0.0.1:8000'
 
