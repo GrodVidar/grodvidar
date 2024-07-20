836d1dd3276205cfdffb4d33f9972916adb4d70d
GrodVidar <vexxart@gmail.com>
2024-07-20 15:57:01 +0200

    testing delivering static files with whitenoise

--- grodvidar/settings/base.py
+++ grodvidar/settings/base.py
@@ -76,9 +76,9 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'grodvidar.wsgi.application'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -141,6 +141,12 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {
             'access_type': 'online',
         }
+    }
+}
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     }
 }
 
--- grodvidar/settings/dev.py
+++ grodvidar/settings/dev.py
@@ -8,10 +8,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
