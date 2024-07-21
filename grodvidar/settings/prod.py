1422b601fcf4dc810228dddc68c0ac84f2bbe0d5
GrodVidar <vexxart@gmail.com>
2024-07-21 13:44:41 +0200

    fix typo in env-variable

--- grodvidar/settings/prod.py
+++ grodvidar/settings/prod.py
@@ -32,10 +32,10 @@ EMAIL_USE_TLS = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_URL = os.environ.get('AWS_URL')
+AWS_BUCKET_URL = os.environ.get('AWS_BUCKET_URL')
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-MEDIA_URL = AWS_URL + '/media/'
+MEDIA_URL = AWS_BUCKET_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
